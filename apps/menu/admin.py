8d1207529a0387fe8bf2feb36ce87df5253e3283
hqpr <adubnyak@gmail.com>
2015-07-23 15:26:52 +0300

    Menu tag #4

--- apps/menu/admin.py
+++ apps/menu/admin.py
@@ -6,6 +6,7 @@ from .models import Menu
 @admin.register(Menu)
 class MenuAdmin(ChangeListOrdering):
     list_display = ('title', 'linked_object')
+    list_display_links = ('title',)
     ordering = ('order',)
 
 
--- apps/menu/templatetags/menu.py
+++ apps/menu/templatetags/menu.py
@@ -1,4 +1,5 @@
 from django import template
+from apps.menu.models import Menu
 
 register = template.Library()
 
@@ -6,6 +7,13 @@ register = template.Library()
 @register.inclusion_tag('menu/get_menu_tag.html', takes_context=True)
 def get_menu(context):
     current_url = context['request']
+    menu = list(Menu.objects.filter(is_active=True))
+    for i, m in enumerate(menu):
+        if i == 0 and current_url.path == '/':
+            m.menu_active = True
+        else:
+            m.menu_active = True if m.get_url() == current_url.path else False
 
     return {
+        'menu': menu
     }
