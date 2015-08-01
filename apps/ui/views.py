94bbb8af478f3863abdba9b2c9c061fc50d07d92
Vadym Zabiiaka <v.zabiiaka@gmail.com>
2015-08-01 14:31:38 +0300

    MainPageView and other view draft's

--- apps/ui/views.py
+++ apps/ui/views.py
@@ -1,17 +1,32 @@
-from django.views.generic import TemplateView, DetailView, ListView
+import importlib
+from django.core.urlresolvers import resolve
+from django.views.generic import View, DetailView
 from apps.page.models import Page
+from apps.category.models import Category
+from apps.menu.models import Menu
 
 
-class MainPageView(TemplateView):
-    template_name = 'base.html'
+class MainPageView(View):
+    def dispatch(self, request, *args, **kwargs):
+        # The first menu is also the main page
+        first_menu_item = Menu.objects.first()
+
+        view_func = resolve(first_menu_item.get_url()).func
+        module = importlib.import_module(view_func.__module__)
+        view = getattr(module, view_func.__name__)
+
+        kwargs['slug'] = first_menu_item.linked_object.get_real_instance().slug
+        return view.as_view()(request, *args, **kwargs)
 
 
 class PageView(DetailView):
+    template_name = 'page.html'
     model = Page
 
 
-class CategoryView(ListView):
-    pass
+class CategoryView(DetailView):
+    template_name = 'category.html'
+    model = Category
 
 
 # handlers
