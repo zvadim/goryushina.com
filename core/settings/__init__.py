f99aafbf533927a5817d611eea01ad06ae62eebf
Vadim <v.zabiiaka@gmail.com>
2015-10-04 11:42:00 -0400

    + gunicorn

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'gunicorn',
     'tinymce',
     'sorl.thumbnail',
     'mce_filebrowser',
